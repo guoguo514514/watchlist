b74b20d5368cb5e931a8a8899b8cde46b045faf6
guoguo wang <gwang@sonicwall.com>
2021-05-20 16:24:14 +0800

    add index page

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import escape, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/home')
+
 @app.route('/index')
-def hello():
-    return '<h1>hello totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html', name=name, movies=movies)
 
 
 @app.route('/user/<name>')
@@ -24,3 +23,19 @@ def test_for_url():
     print(url_for('test_for_url'))
     print(url_for('test_for_url', num=2))
     return 'test page'
+
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
